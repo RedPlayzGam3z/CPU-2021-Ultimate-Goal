2ea53abfd0785dddc4be39c2f3a7ea3c60d155d1
PilotSamBevans <PilotSamBevans@gmail.com>
2020-10-06 16:18:38 -0500

    Hii

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoodBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoodBotTeleop.java
@@ -34,7 +34,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
-
+//Hiii
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
 /**
